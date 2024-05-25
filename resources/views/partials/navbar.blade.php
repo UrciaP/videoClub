36859768ab8edc0875a1aa12681fea9ec2dceb5c
Urcia-Tw <urciapelaezluisalex@gmail.com>
2024-05-24 22:01:40 -0500

    ya tu saes

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/" style="color:#777"><span style="font-size:15pt">&#9820;</span> Videoclub</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        @if( true || Auth::check() )
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ Request::is('catalog') && ! Request::is('catalog/create')? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/catalog')}}">
+                            <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
+                            Catálogo
+                        </a>
+                    </li>
+                    <li class="nav-item {{  Request::is('catalog/create') ? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/catalog/create')}}">
+                            <span>&#10010</span> Nueva película
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <form action="{{ url('/logout') }}" method="POST" style="display:inline">
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-link nav-link" style="display:inline;cursor:pointer">
+                                Cerrar sesión
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        @endif
+    </div>
+</nav>
+
+
+
+
+<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">VideoClub</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="catalog">Catálogo</a>
+                <a class="nav-link" href="login">Login</a>
+                <a class="nav-link" href="logout">Logout</a>
+            </div>
+        </div>
+    </div>
+</nav> -->
